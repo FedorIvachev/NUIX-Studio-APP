d64833472d2f90219e34b254c21a61425f070aec
FedorIvachev <feodor.ivachev@gmail.com>
2021-04-25 02:29:31 +0300

    Color widget

---
+++ Assets/NUIX-Studio-Client/openHABIntegration/Helpers/Color/ColorPickerHelper.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit.Experimental.ColorPicker;
+
+
+public class ColorPickerHelper : MonoBehaviour
+{
+    public ColorPicker colorPicker;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
